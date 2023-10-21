e117412d565cba64017cdc49e7313ef9e69adafb
LadyCharmine <130120026+LadyCharmine@users.noreply.github.com>
2023-10-21 18:15:16 +0700

    UAS Pemrograman Berorientasi Objek

---
+++ src/pbopertemuan5/Perpustakaan.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbopertemuan5;
+
+/**
+ *
+ * @author bayuo
+ */
+public class Perpustakaan {
+    private String id_buku;
+    private String isbn;
+    private String judul_buku;
+    private String tgl_beli;
+    private String penerbit;
+
+    /**
+     * @return the idBuku
+     */
+    public String getid_buku() {
+        return id_buku;
+    }
+
+    /**
+     * @param idBuku the idBuku to set
+     */
+    public void setid_buku(String id_buku) {
+        this.id_buku = id_buku;
+    }
+
+    /**
+     * @return the isbn
+     */
+    public String getisbn() {
+        return isbn;
+    }
+
+    /**
+     * @param isbn the isbn to set
+     */
+    public void setisbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    /**
+     * @return the judulBuku
+     */
+    public String getjudul_buku() {
+        return judul_buku;
+    }
+
+    /**
+     * @param judulBuku the judulBuku to set
+     */
+    public void setjudul_buku(String judul_buku) {
+        this.judul_buku = judul_buku;
+    }
+
+    /**
+     * @return the tglBeli
+     */
+    public String gettgl_beli() {
+        return tgl_beli;
+    }
+
+    /**
+     * @param tglBeli the tglBeli to set
+     */
+    public void settgl_beli(String tgl_beli) {
+        this.tgl_beli = tgl_beli;
+    }
+
+    /**
+     * @return the penerbit
+     */
+    public String getpenerbit() {
+        return penerbit;
+    }
+
+    /**
+     * @param penerbit the penerbit to set
+     */
+    public void setpenerbit(String penerbit) {
+        this.penerbit = penerbit;
+    }
+
+    
+}
